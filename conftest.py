e35edc71f4cbe0094f087743e1a4415cc50e2afa
Lukevich <luckevichartem@gmail.com>
2024-10-23 17:57:01 +0300

    changed Dockerfile

--- conftest.py
+++ conftest.py
@@ -7,8 +7,10 @@ from faker import Faker
 
 def get_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=old")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--log-level=3")
     options.add_argument("--disable-search-engine-choice-screen")
     options.add_argument("--disable-blink-features=AutomationControlled")
@@ -24,8 +26,6 @@ def driver(request):
     driver = get_driver()
     request.cls.driver = driver
     yield
-    if os.path.exists(os.path.join(os.getcwd(), "cookies", "user-cookies.pkl")):
-        os.remove(os.path.join(os.getcwd(), "cookies", "user-cookies.pkl"))
     driver.quit()
 
 
